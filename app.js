eb38ef226246fc696537f655ff39144f041ada10
alamgir hossain <alamgir555hossain@gmail.com>
2020-04-04 00:25:19 -0400

    users route, morgan, middleware

--- app.js
+++ app.js
@@ -1,46 +1,139 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+app.use(morgan('dev'));
+
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//     res.status(404).json({message: 'Hello from the server side!', app: 'Natours'});
-// });
+app.use((req, res, next) => {
+    console.log('Meddleware is there');
+    next();
+});
 
-// app.post('/', (req, res) => {
-//     res.send('You can post to thid endpoit..');
-// })
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
+});
 
 const tours = JSON.parse( fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours =  (req, res) => {
+    console.log(req.requestTime);
     res.status(200).json({
         status: 'success',
+        requestTime: req.requestTime,
         results: tours.length,
         data: {
             tours      
         }
     });
-}); 
+};
 
-app.post('/api/v1/tours', (req, res) => {
-   const newId = tours[tours.length - 1].id + 1;
-   const newTour = Object.assign({id: newId }, req.body);
-
-   tours.push(newTour);
-   fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours),
-    error => {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tours: newTour
-            }
+const getTour = (req, res) => {
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+    // if(id > tours.length){
+       if(!tour){ 
+        return res.status(404).json({
+            status: 'error',
+            message: 'Id is invaild'
         })
     }
-   )
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    });
+};
 
-});
+const createTour = (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newId }, req.body);
+ 
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours),
+     error => {
+         res.status(201).json({
+             status: 'success',
+             data: {
+                 tours: newTour
+             }
+         })
+     }
+    )
+ };
+
+ const updateTour = (req, res) => {
+    if(req.params.id * 1 > tours.length){
+        return res.status(404).json({
+            status: 'error',
+            message: 'This error'
+        });
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: '<Update tour here...>'
+        }
+    }); 
+};
+
+const deleteTour =  (req, res) => {
+    if(req.params.id * 1 > tours.length){
+        return res.status(404).json({
+            status: 'error',
+            message: 'NO data'
+        })
+    }
+    res.status(204).json({
+        status: 'success',
+        data: null
+    })
+}
+
+const getAllUsers = (req, res) => {
+    res.status(500).json({
+        status: 'fail',
+        message: 'This is error'
+    });
+}
+
+const createUsers = (req, res) => {
+    res.status(500).json({
+        status: 'fail',
+        message: 'This is error'
+    });
+}
+
+const getUser = (req, res) => {
+    res.status(500).json({
+        status: 'fail',
+        message: 'This is error'
+    });
+}
+
+const updateUser = (req, res) => {
+    res.status(500).json({
+        status: 'fail',
+        message: 'This is error'
+    });
+}
+
+const deleteUser = (req, res) => {
+    res.status(500).json({
+        status: 'fail',
+        message: 'This is error'
+    });
+}
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour);
+
+app.route('/api/v1/users').get(getAllUsers).post(createUsers);
+app.route('/api/v1/users/:user').get(getUser).patch(updateUser).delete(deleteUser);
 
 const port = 3000;
 app.listen(port, () => {
