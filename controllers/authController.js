01cac7e5db004ddd1b4cc958a85405b6d2710d83
alamgir hossain <alamgir555hossain@gmail.com>
2020-08-10 22:58:11 -0400

    api-login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const JWT = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -22,3 +23,17 @@ exports.signup = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password', 400));
+  }
+  const token = '';
+
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+};
