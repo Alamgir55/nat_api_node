33691b1d4692e4d841a5cee0aa545bea7341d27c
alamgir hossain <alamgir555hossain@gmail.com>
2020-12-14 20:48:26 -0500

    forget-reset-pass

--- utils/email.js
+++ utils/email.js
@@ -15,7 +15,14 @@ module.exports = class Email {
   newTransport() {
     if (process.env.NODE_ENV === 'production') {
       // Sendgrid
-      return 1;
+      return nodemailer.createTransport({
+        // Sendgrid
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD,
+        },
+      });
     }
     return nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
@@ -52,5 +59,12 @@ module.exports = class Email {
 
   async sendWelcome() {
     await this.send('welcome', 'Welcome to the our business');
+  }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token (valid for noly 10 minutes)',
+    );
   }
 };
