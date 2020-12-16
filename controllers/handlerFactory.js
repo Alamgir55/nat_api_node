0d26ff34122a4906ca3198f26b8a5d6e2181e0e5
alamgir hossain <alamgir555hossain@gmail.com>
2020-12-15 21:51:10 -0500

    payment-checkout

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -77,7 +77,8 @@ exports.getAll = (Model) =>
       .sort()
       .limitFields()
       .Paginate();
-    const doc = await features.query.explain();
+    //const doc = await features.query.explain();
+    const doc = await features.query;
 
     res.status(200).json({
       status: 'success',
--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,7 @@
 import { login, logout } from './login';
 import { displayMap } from './mapbox';
 import { updateSettings } from './updateSettings';
+import { bookTour } from './stripe';
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
@@ -10,6 +11,7 @@ const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('#form-user-password');
+const bookBtn = document.getElementById('book-tour');
 
 if(mapBox){
     const locations = JSON.parse(mapBox.dataset.locations);
@@ -52,3 +54,10 @@ if (userPasswordForm) userPasswordForm.addEventListener('submit', async e => {
     document.getElementById('password').value = '';
     document.getElementById('password-confirm').value = '';
 });
+
+    if(bookBtn)
+        bookBtn.addEventListener('click', e => {
+            e.target.textContent = 'Processing....';
+            const {tourId} = e.target.dataset;
+            bookTour(tourId);
+        })
